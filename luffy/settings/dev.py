0e929398858c6bcb003223ae9a16ead8d1ef88bd
xuxu <j18682948872@163.com>
2019-10-28 16:15:07 +0800

    异常处理

--- luffy/settings/dev.py
+++ luffy/settings/dev.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'luffy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music',
+        'USER': 'ceshi001',
+        'PASSWORD': 'ceshi001',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
+# }
 }
 
 
